22ef20df0481a0a743ac90cb3d7012b5c875e994
taoteh1221 <mike@dragonfrugal.com>
2023-02-20 18:36:06 -0500

    sys-temp
    sys-temp

---
+++ sys-temp.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+get_system_temp () {
+
+temp=$(echo $(cat /sys/devices/virtual/thermal/thermal_zone0/temp) /1000 | bc)
+
+echo "${temp}c"
+
+}
+
+while sleep 1; do get_system_temp; done
+
+
