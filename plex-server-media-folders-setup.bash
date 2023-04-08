1a39b1786927d5de4cb30c6e957aff650e662730
taoteh1221 <mike@dragonfrugal.com>
2023-04-08 03:21:12 -0400

    fix
    fix

--- plex-server-media-folders-setup.bash
+++ plex-server-media-folders-setup.bash
@@ -6,6 +6,6 @@ sudo mkdir -p /media/$USER/Transcoding
 sudo mkdir -p /media/$USER/Movies
 sudo mkdir -p /media/$USER/TV
 sudo chown -R $USER:$MYGROUP /media/$USER
-sudo chmod 750 /media/$USER
-sudo setfacl -m g:$MYGROUP:rwx /media/$USER
+sudo chmod -R 750 /media/$USER
+sudo setfacl -R -m g:$MYGROUP:rwx /media/$USER
 sudo service plexmediaserver restart
