b1a7fc86e788e6ad3bc428e8b8441e35c8e7ac77
taoteh1221 <mike@dragonfrugal.com>
2023-08-08 12:32:55 -0400

    plex folders setup script
    plex folders setup script

--- plex-server-media-folders-setup.bash
+++ plex-server-media-folders-setup.bash
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-MYGROUP="$USER"
-sudo usermod -a -G $MYGROUP plex
-sudo mkdir -p /media/$USER/Transcoding
-sudo mkdir -p /media/$USER/Movies
-sudo mkdir -p /media/$USER/TV
-sudo chown -R $USER:$MYGROUP /media/$USER
-sudo chmod -R 750 /media/$USER
-sudo setfacl -R -m g:$MYGROUP:rwx /media/$USER
+
+# Configs
+
+BASE_DIR="/media/taoteh1221/1tb_HDD_Disk/Plex-Videos"
+
+USER_GROUP="$USER"
+
+
+# Add user to plex group
+sudo usermod -a -G $USER_GROUP plex
+
+
+# Create the directory structure
+sudo mkdir -p $BASE_DIR/Transcoding
+sudo mkdir -p $BASE_DIR/Movies
+sudo mkdir -p $BASE_DIR/TV
+
+sleep 1
+
+# Set permissions
+sudo chown -R $USER_GROUP:$USER_GROUP $BASE_DIR
+sudo chmod -R 750 $BASE_DIR
+sudo setfacl -R -m g:$USER_GROUP:rwx $BASE_DIR
+
+sleep 1
+
+# Restart media server
 sudo service plexmediaserver restart
+
+
+
+
+
