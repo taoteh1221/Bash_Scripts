7e0002dcb3f9780fc68c69b931d0ca27a9ca32ed
taoteh1221 <mike@dragonfrugal.com>
2024-11-11 00:09:30 -0500

    fedora setup revs
    fedora setup revs

--- Fedora-Setup.bash
+++ Fedora-Setup.bash
@@ -23,14 +23,24 @@
 
 # Config
 
-PREFERRED_HOSTNAME="taoteh1221-Desktop-Asus-Lin"
+PREFERRED_HOSTNAME="taoteh1221-Server-Rock5b-Lin"
 
 SECONDS_TO_SHOW_BOOT_MENU=10
+
+ENABLE_COCKPIT_REMOTE_ADMIN="no" # "no" / "yes"
+
+HEADLESS_SETUP_ONLY="yes" # "no" / "yes"
 
 # END Config
 
 
 ######################################
+
+# Are we running on an ARM-based CPU?
+IS_ARM=$(uname -r | grep "aarch64")
+
+######################################
+
 
 # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
 
@@ -66,16 +76,25 @@ sudo dnf upgrade -y
 
 sleep 3
 
-# Install building tools
-sudo dnf install -y --skip-broken kernel-devel-`uname -r` kernel-headers kernel-devel gcc make dkms acpid akmods pkgconfig elfutils-libelf-devel qt5-qtx11extras
+# Install building / system tools
+sudo dnf install -y --skip-broken kernel-devel-`uname -r` kernel-headers kernel-devel gcc make dkms acpid akmods pkgconfig elfutils-libelf-devel
+####
+sudo dnf group install -y --skip-broken c-development container-management d-development development-tools rpm-development-tools
 
-# Install build + dev tools / games / media support / etc
-sudo dnf group install -y --skip-broken 3d-printing audio c-development container-management editors d-development development-tools games rpm-development-tools sound-and-video vlc
+# Install samba tools
+sudo dnf install -y cifs-utils
 
-sleep 3
+# Install home directory encryption tools, openssl
+sudo dnf install -y --skip-broken ecryptfs-utils openssl
+
+# Install uboot tools (for making ARM disk images bootable, if device is NOT supported by arm-image-installer)
+sudo dnf install -y --skip-broken uboot-tools uboot-images-armv8 rkdeveloptool gdisk
+
+# IOT (ARM CPU) image installer (fedora raspi images to microsd, etc)
+sudo dnf install -y arm-image-installer
 
 
-# If we are DELETING a MOK (Machine Owner Key), for seure boot module signing
+# If we are DELETING a MOK (Machine Owner Key), for secure boot module signing
 if [ "$1" == "reset_secureboot_mok" ]; then
 
 echo " "
@@ -101,11 +120,6 @@ elif [ "$1" == "enroll_secureboot_mok" ]; then
     
      # Check to see if MOK keys have already been setup
      if [ ! -f "/var/lib/shim-signed/mok/MOK.priv" ] && [ ! -f "/var/lib/shim-signed/mok/MOK.der" ]; then
-
-     # Make sure we have openssl installed
-     sudo dnf install -y openssl
-
-     sleep 3
 
      sudo mkdir -p /var/lib/shim-signed/mok
 
@@ -137,8 +151,19 @@ elif [ "$1" == "sign_secureboot_modules" ]; then
 
 sudo akmods --force --rebuild
 
+echo " "
+echo "${red}REBOOT YOUR MACHINE ONE LAST TIME, TO ALLOW LOADING ALL MODULES IN SECURE BOOT MODE."
+echo "${reset} "
+echo "Finished module-signing setup..."
+echo " "
+
+exit
+
 fi
 # END CLI params logic
+
+
+sleep 3
 
 
 # Check to see if MOK secure boot module signing KEYS have already been setup
@@ -157,6 +182,44 @@ exit
 fi
 
 
+# If we are enabling cockpit, for remote admin UI ability
+if [ "$ENABLE_COCKPIT_REMOTE_ADMIN" == "yes" ]; then
+
+sudo dnf install -y cockpit
+
+sleep 3
+
+sudo systemctl enable --now cockpit.socket
+
+sudo firewall-cmd --add-service=cockpit
+
+sudo firewall-cmd --add-service=cockpit --permanent
+
+fi
+
+
+# Set default (user) editors to nano
+DEFAULT_EDITOR_CHECK=$(sed -n '/export EDITOR/p' ~/.bash_profile)
+DEFAULT_VISUAL_CHECK=$(sed -n '/export VISUAL/p' ~/.bash_profile)
+
+
+if [ "$DEFAULT_EDITOR_CHECK" == "" ]; then
+bash -c 'echo "export EDITOR=nano" >> ~/.bash_profile'
+else
+sed -i 's/export EDITOR=.*/export EDITOR=nano/g' ~/.bash_profile > /dev/null 2>&1
+fi
+
+
+if [ "$DEFAULT_VISUAL_CHECK" == "" ]; then
+bash -c 'echo "export VISUAL=nano" >> ~/.bash_profile'
+else
+sed -i 's/export VISUAL=.*/export VISUAL=nano/g' ~/.bash_profile > /dev/null 2>&1
+fi
+
+
+# Set hostname
+sudo hostnamectl set-hostname $PREFERRED_HOSTNAME
+
 # Secure user home directory, from other accounts snooping it
 sudo chmod 750 /home/$USER
 
@@ -165,6 +228,8 @@ sudo chmod 750 /home/$USER
 timedatectl set-local-rtc 0
 # As admin too
 sudo timedatectl set-local-rtc 0
+
+sleep 3
 
 # Enable FUSION repos
 sudo dnf install -y \
@@ -189,11 +254,21 @@ sudo systemctl start crond.service
 
 sleep 3
 
-# Install generic graphics card libraries
-sudo dnf install -y libglvnd-glx libglvnd-opengl libglvnd-devel
+
+##############################################################################
+##############################################################################
+
+# If we are doing a graphical interface setup (NOT headless / terminal-only)
+if [ "$HEADLESS_SETUP_ONLY" == "no" ]; then
+
+# Install games / media support / etc
+sudo dnf group install -y --skip-broken audio 3d-printing editors games sound-and-video vlc
+
+# Install generic graphics card libraries, and other interface-related libraries
+sudo dnf install -y --skip-broken libglvnd-glx libglvnd-opengl libglvnd-devel qt5-qtx11extras
 
 # Install cinnamon desktop
-sudo dnf install -y @cinnamon-desktop-environment 
+sudo dnf install -y @cinnamon-desktop-environment
 
 #Install KDE
 sudo dnf install -y @kde-desktop
@@ -204,41 +279,29 @@ sleep 3
 KDE_MOUSE_CHECK=$(sed -n '/DoubleClickInterval/p' ~/.config/kdeglobals)
 
 
-if [ "$KDE_MOUSE_CHECK" == "" ]; then
-# Place directly below [KDE], if it does NOT exist
-sed -i '/\[KDE\]/a DoubleClickInterval=1000' ~/.config/kdeglobals > /dev/null 2>&1
-else
-# Replace with preferred setting, if exists
-sed -i 's/DoubleClickInterval=.*/DoubleClickInterval=1000/g' ~/.config/kdeglobals > /dev/null 2>&1
-fi
+    if [ "$KDE_MOUSE_CHECK" == "" ]; then
+    # Place directly below [KDE], if it does NOT exist
+    sed -i '/\[KDE\]/a DoubleClickInterval=1000' ~/.config/kdeglobals > /dev/null 2>&1
+    else
+    # Replace with preferred setting, if exists
+    sed -i 's/DoubleClickInterval=.*/DoubleClickInterval=1000/g' ~/.config/kdeglobals > /dev/null 2>&1
+    fi
 
 
 # Install dropbox for nemo / dolphin (file explorers)
-sudo dnf install -y nemo-dropbox dolphin-plugins
+sudo dnf install -y --skip-broken nemo-dropbox dolphin-plugins
 
 #Install LXDE
 sudo dnf group install -y lxde-desktop
 
 # Install preferred file archiving tools
-sudo dnf install -y p7zip p7zip-plugins unrar ark engrampa
+sudo dnf install -y --skip-broken p7zip p7zip-plugins unrar ark engrampa
 
 # Install preferred disk tools
 sudo dnf install -y gparted
 
-# Install samba tools
-sudo dnf install -y cifs-utils
-
-# Install home directory encryption tools, openssl
-sudo dnf install -y ecryptfs-utils openssl
-
 # Install 'passwords and keys' and Kgpg (GPG import / export) interfaces
-sudo dnf install -y seahorse kgpg
-
-# Install uboot tools (for making ARM disk images bootable, if device is NOT supported by arm-image-installer)
-sudo dnf install -y uboot-tools uboot-images-armv8 rkdeveloptool gdisk
-
-# IOT (ARM CPU) image installer (fedora raspi images to microsd, etc)
-sudo dnf install -y arm-image-installer
+sudo dnf install -y --skip-broken seahorse kgpg
 
 # Install official google chrome (if you "enabled 3rd party repositories" during OS installation),
 # AND evolution email / calendar
@@ -252,33 +315,39 @@ sudo dnf install -y libxcrypt-compat
 sudo dnf install -y --skip-broken VirtualBox virt-manager edk2-ovmf swtpm-tools spice-vdagent
 
 # Install darkplaces-quake, steam, AND lutris
-sudo dnf install -y darkplaces-quake darkplaces-quake-server steam lutris
-
-# Set default editors to nano
-DEFAULT_EDITOR_CHECK=$(sed -n '/export EDITOR/p' ~/.bash_profile)
-DEFAULT_VISUAL_CHECK=$(sed -n '/export VISUAL/p' ~/.bash_profile)
-
-
-if [ "$DEFAULT_EDITOR_CHECK" == "" ]; then
-bash -c 'echo "export EDITOR=nano" >> ~/.bash_profile'
-else
-sed -i 's/export EDITOR=.*/export EDITOR=nano/g' ~/.bash_profile > /dev/null 2>&1
-fi
-
-
-if [ "$DEFAULT_VISUAL_CHECK" == "" ]; then
-bash -c 'echo "export VISUAL=nano" >> ~/.bash_profile'
-else
-sed -i 's/export VISUAL=.*/export VISUAL=nano/g' ~/.bash_profile > /dev/null 2>&1
-fi
-
+sudo dnf install -y --skip-broken darkplaces-quake darkplaces-quake-server steam lutris
 
 # Disable sleep mode, IF NOBODY LOGS IN VIA INTERFACE
 # https://discussion.fedoraproject.org/t/gnome-suspends-after-15-minutes-of-user-inactivity-even-on-ac-power/79801
 sudo -u gdm dbus-run-session gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0 > /dev/null 2>&1
 
-# Set hostname
-sudo hostnamectl set-hostname $PREFERRED_HOSTNAME
+# If running a geforce graphics card, install the drivers
+NVIDIA_GEFORCE=$(lspci | grep -Ei 'GeForce')
+
+
+    if [ "$NVIDIA_GEFORCE" != "" ]; then
+
+    #https://discussion.fedoraproject.org/t/nvidia-drivers-with-secure-boot-no-longer-working/84444
+    sudo dnf reinstall -y linux-firmware
+
+    sleep 3
+
+    sudo dnf install -y --skip-broken akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686
+
+    sleep 3
+
+    # https://forums.developer.nvidia.com/t/major-kde-plasma-desktop-frameskip-lag-issues-on-driver-555/293606
+    # https://download.nvidia.com/XFree86/Linux-x86_64/510.60.02/README/gsp.html
+    sudo grubby --update-kernel=ALL --args=nvidia.NVreg_EnableGpuFirmware=0
+
+    fi
+
+
+fi
+
+##############################################################################
+##############################################################################
+
 
 # Make grub boot menu ALWAYS SHOW (even on NON-dual-boot setups)
 sudo grub2-editenv - unset menu_auto_hide
@@ -299,27 +368,6 @@ if [ "$OS_PROBER_CHECK" == "" ]; then
 sudo bash -c 'echo "GRUB_DISABLE_OS_PROBER=true" >> /etc/default/grub'
 else
 sudo sed -i 's/GRUB_DISABLE_OS_PROBER=.*/GRUB_DISABLE_OS_PROBER=true/g' /etc/default/grub > /dev/null 2>&1
-fi
-
-
-# If running a geforce graphics card, install the drivers
-NVIDIA_GEFORCE=$(lspci | grep -Ei 'GeForce')
-
-
-if [ "$NVIDIA_GEFORCE" != "" ]; then
-
-#https://discussion.fedoraproject.org/t/nvidia-drivers-with-secure-boot-no-longer-working/84444
-sudo dnf reinstall -y linux-firmware
-
-sleep 3
-
-sudo dnf install -y --skip-broken akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-libs.i686
-
-sleep 3
-
-# https://forums.developer.nvidia.com/t/major-kde-plasma-desktop-frameskip-lag-issues-on-driver-555/293606
-sudo grubby --update-kernel=ALL --args=nvidia.NVreg_EnableGpuFirmware=0
-
 fi
 
 
@@ -345,6 +393,8 @@ if [ "$MOK_SETUP" = "1" ]; then
 
 echo " "
 echo "${red}YOU *MUST* NOW REBOOT YOUR COMPUTER, INITIATE 'MOK Management', CHOOSE 'Enroll MOK' -> 'Continue', ENTER THE PIN YOU CREATED / REBOOT, to enable MOK module signing!"
+echo "AFTER REBOOTING, RERUN this script with the 'sign_secureboot_modules' parameter, TO ENABLE SECURE BOOT ON ALL MODULES:"
+echo "${cyan}./Fedora-Setup.bash sign_secureboot_modules"
 echo "${reset} "
 
 fi
