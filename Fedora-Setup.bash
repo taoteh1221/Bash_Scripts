819597a1dec0177827a2d625b62dd144053b9179
taoteh1221 <mike@dragonfrugal.com>
2024-11-18 23:10:46 -0500

    fedora setup revs
    fedora setup revs

--- Fedora-Setup.bash
+++ Fedora-Setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2024 GPLv3, Open Crypto Tracker by Mike Kilday: Mike@DragonFrugal.com (leave this copyright / attribution intact in ALL forks / copies!)
+# Copyright 2024 GPLv3, by Mike Kilday: Mike@DragonFrugal.com (leave this copyright / attribution intact in ALL forks / copies!)
 
 ####
 # USAGE:
